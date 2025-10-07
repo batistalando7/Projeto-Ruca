ce09e436db8a2a4265cd9eaf99336929ae4aaac7
unknown <Administrador@Desenvolvedor10.infosi.local>
2025-10-07 10:34:31 +0100

    front definitivo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Main from "./components/layouts/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
