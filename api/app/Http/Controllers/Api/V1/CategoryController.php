8702702f6ef12523de83aaf77af3c09fc75e632b
batistalando7 <batistalando7@gmail.com>
2025-10-07 20:28:17 +0100

    subindo logotipo e menu atualizado

---
+++ api/app/Http/Controllers/Api/V1/CategoryController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        Category::all();
+        return response()->json(Category::all(), 200);
+    }
+    public function store(Request $request)
+    {
+        $category = Category::create($request->all());
+        return response()->json($category, 201);
+    }
+    public function show(Category $category)
+    {
+        return $category;
+    }
+    public function update(Request $request, Category $category)
+    {
+        $category->update($request->all());
+        return response()->json($category, 200);
+    }
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json(null, 204);
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -7,5 +7,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 }); */
+/* User ruote */
+Route::apiResource('/V1/users', 'Api\V1\UserController');
 
-Route::apiResource('users', 'Api\UserController');
+/* Category route*/
+Route::apiResource('/V1/categories', 'Api\V1\CategoryController');
+
+/* Service route */
+Route::apiResource('/V1/services', 'Api\V1\ServiceController');
+
+/* Invoice route */
+Route::apiResource('/V1/invoices', 'Api\V1\InvoiceController');
+
